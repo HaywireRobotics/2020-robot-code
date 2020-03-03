2b6923b25e4a246bee39d512312acf69808f0422
Brenek Harrison <brenekharrison@gmail.com>
2020-03-03 14:31:51 -0700

    Advanced Auto Builds

--- src/main/kotlin/frc/robot/commands/LaunchIonCannonForTimey.kt
+++ src/main/kotlin/frc/robot/commands/LaunchIonCannonForTimey.kt
@@ -1,4 +1,4 @@
-a/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -13,7 +13,7 @@ import frc.robot.subsystems.TurboLiftSubsystem
 import edu.wpi.first.wpilibj2.command.CommandBase
 import edu.wpi.first.wpilibj.Timer
 
-class LaunchIonCannonForTimey(val topTargetRate: Number, val bottomTargetRate: Number, val ionCannon: IonCannony, val turboLift: TurboLiftSubsystem) : CommandBase() {
+class LaunchIonCannonForTimey(val topTargetRate: Number, val bottomTargetRate: Number, val shootTime: Number, val ionCannon: IonCannony, val turboLift: TurboLiftSubsystem) : CommandBase() {
   /**
    * Creates a new LaunchIonCannonForTimey.
    *
@@ -48,6 +48,6 @@ class LaunchIonCannonForTimey(val topTargetRate: Number, val bottomTargetRate: N
 
   // Returns true when the command should end.
   override fun isFinished(): Boolean {
-      return timer.hasPeriodPassed(10)
+      return timer.hasPeriodPassed(shootTime.toDouble())
   }
 }
--- src/main/kotlin/frc/robot/commands/ShootyDriveyAuto.kt
+++ src/main/kotlin/frc/robot/commands/ShootyDriveyAuto.kt
@@ -9,22 +9,20 @@ package frc.robot.commands
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.CommandBase
-import edu.frc.robot.commands.TurretSeek
-import edu.frc.robot.commands.DriveForTime
-import edu.frc.robot.commands.LaunchIonCannonForTimey
-import edu.frc.robot.subsystems.TurretSubsystem
-import edu.frc.robot.subsystems.TurboLiftSubsystem
-import edu.frc.robot.subsystems.HyperdriveSubsystem
-import edu.frc.robot.subsystems.IonCannony
+import frc.robot.commands.TurretSeek
+import frc.robot.commands.DriveForTime
+import frc.robot.commands.LaunchIonCannonForTimey
+import frc.robot.subsystems.TurretSubsystem
+import frc.robot.subsystems.TurboLiftSubsystem
+import frc.robot.subsystems.HyperdriveSubsystem
+import frc.robot.subsystems.IonCannony
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 class ShootyDriveyAuto(turret: TurretSubsystem, hyperDrive: HyperdriveSubsystem, ionCannon: IonCannony, turboLift: TurboLiftSubsystem) : SequentialCommandGroup() {
   init {
-
-    
     // Add your commands in the super() call, e.g.
     // super(FooCommand(), BarCommand())
-    super()
+    addCommands(TurretSeek(turret), LaunchIonCannonForTimey(150000, 150000, 10, ionCannon, turboLift), DriveForTime(hyperDrive, 0.3, 0.5))
   }
 }
